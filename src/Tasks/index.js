8fe3846f424f8eacb1c7e0877716158c44be0d07
Paulina Piotrzkowska <paulina.piotrzkowska@gmail.com>
2023-11-02 00:06:12 +0100

    changed

---
+++ src/Tasks/index.js
@@ -0,0 +1,33 @@
+import "./style.css";
+
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
+    <ul className="task">
+        {tasks.map(task => (
+            <li
+                className={`task__item${task.done && hideDone
+                    ? " task__item--hidden"
+                    : ""}`}
+            >
+                <button
+                    className="task__button task__button--done"
+                    onClick={() => toggleTaskDone(task.id)}
+                >
+                    {task.done ? "✔" : ""}
+                </button>
+                <span className={
+                    `task__content${task.done ? " task__content--done" : ""}`
+                }>
+                    {task.id} - {task.content} 
+                </span>
+                <button
+                    className="task__button task__button--remove"
+                    onClick={() => removeTask(task.id)}
+                >
+                    🗑
+                </button>
+            </li>
+        ))}
+    </ul>
+);
+
+export default Tasks;
